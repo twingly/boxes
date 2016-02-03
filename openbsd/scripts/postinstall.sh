5a6e5a7794d88f2957ea873d1a48188fef275df3
Patrik Ragnarsson <patrik@starkast.net>
2016-02-03 01:36:47 +0100

    Update OpenBSD box to 5.8

--- openbsd/scripts/postinstall.sh
+++ openbsd/scripts/postinstall.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-export PKG_PATH="$MIRROR/pub/OpenBSD/`uname -r`/packages/`arch -s`/"
+uname_r=`uname -r`
+
+export PKG_PATH="$MIRROR/pub/OpenBSD/$uname_r/packages/`arch -s`/"
 
 # set pkg path for users
 echo "export PKG_PATH=\"$PKG_PATH\"" >> /root/.profile
 echo "export PKG_PATH=\"$PKG_PATH\"" >> /home/vagrant/.profile
 
-# install wget/curl
-pkg_add wget curl
+# install sudo, required by Vagrant
+pkg_add sudo--
+
+# passwordless sudo for Vagrant
+echo "vagrant ALL=(ALL) NOPASSWD: SETENV: ALL" >> /etc/sudoers
 
 # ansible support
 pkg_add -z python-2
-
-# sudo
-echo "vagrant ALL=(ALL) NOPASSWD: SETENV: ALL" >> /etc/sudoers
