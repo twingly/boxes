ccfab9463968d4066458078503ba4a2973ce949f
Patrik Ragnarsson <patrik@starkast.net>
2023-11-12 18:51:53 +0100

    Install any available patches when building the box

--- openbsd/scripts/postinstall.sh
+++ openbsd/scripts/postinstall.sh
@@ -19,3 +19,8 @@ hostname=$(hostname -s)
 printf "%s\n" "$hostname" > /etc/myname
 printf "127.0.0.1\tlocalhost %s\n" "$hostname" > /etc/hosts
 printf "::1\t\tlocalhost %s\n" "$hostname" >> /etc/hosts
+
+# list and install available patches
+syspatch -c
+# handle: "syspatch: updated itself, run it again to install missing patches"
+syspatch || syspatch || true
